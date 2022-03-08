fc240d89cf33e8242143bed0b5b3713dadd3c507
MinuChoi <96402772+CODEMANMINU@users.noreply.github.com>
2022-03-08 12:34:58 +0900

    nav
    나브적용했습니다

---
+++ My project/Assets/Scripts/MonsterAi.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterAi : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public GameObject target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nav.destination != target.transform.position)
+        {
+            nav.SetDestination(target.transform.position);
+        }
+        else
+        {
+            nav.SetDestination(transform.position);
+        }
+    }
+}
